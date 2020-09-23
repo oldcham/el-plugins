f4d183e37b6f3800e31603109be46f34fd144fbe
Elli-tt <elliottjoiner@me.com>
2020-09-23 16:04:21 +0100

    ouraniaaltar: version increase

--- ouraniaaltar/ouraniaaltar.gradle.kts
+++ ouraniaaltar/ouraniaaltar.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.0.3"
+version = "1.0.4"
 
 project.extra["PluginName"] = "El ZMI" // This is the name that is used in the external plugin manager panel
 project.extra["PluginDescription"] = "Crafts runes for you" // This is the description that is used in the external plugin manager panel
